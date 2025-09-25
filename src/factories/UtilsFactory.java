e3e0f9377923b3b1705a986a4d1ac83f90015e51
jad-png <j.belassiria@uca.ca.ma>
2025-09-25 16:30:26 +0100

    feat():CRYP-95 CRYP-96 Create factory class for utilities classes that create new instances for transactions and wallet utils based on method choice

---
+++ src/factories/UtilsFactory.java
@@ -0,0 +1,28 @@
+package factories;
+
+import repository.interfaces.ITransactionRepository;
+import repository.interfaces.IWalletRepository;
+import utils.TransactionUtils;
+import utils.WalletUtils;
+
+public class UtilsFactory {
+    private final ITransactionRepository txRepo;
+    private final IWalletRepository wtRepo;
+
+    public UtilsFactory(ITransactionRepository txRepo, IWalletRepository wtRepo) {
+        this.txRepo = txRepo;
+        this.wtRepo = wtRepo;
+    }
+
+    public TransactionUtils createTransactionUtils() {
+        return new TransactionUtils(txRepo);
+    }
+
+    public WalletUtils createWalletUtils() {
+        return new WalletUtils(wtRepo);
+    }
+
+    public static UtilsFactory getInstance(ITransactionRepository txRepo, IWalletRepository wtRepo) {
+        return new UtilsFactory(txRepo, wtRepo);
+    }
+}
