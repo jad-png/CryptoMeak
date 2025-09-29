dde19bdb860c6af725d5f3c034bc857884f167b6
jad-png <j.belassiria3993@uca.ac.ma>
2025-09-29 13:03:34 +0200

    feat(): implemented controller for auth

---
+++ src/controller/AuthController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.util.Optional;
+
+import model.Wallet;
+import service.impl.AuthServiceImpl;
+import service.result.AuthResult;
+
+public class AuthController {
+    private final AuthServiceImpl authSer;
+
+    public AuthController(AuthServiceImpl authSer) {
+        this.authSer = authSer;
+    }
+
+    public AuthResult registerWallet(Wallet wt, String pswrd) {
+        return authSer.registerWallet(wt, pswrd);
+    }
+
+    public AuthResult login(String wtAddress, String pswrd) {
+        return authSer.login(wtAddress, pswrd);
+    }
+
+    public boolean logout(String sessionId) {
+        return authSer.logout(sessionId);
+    }
+
+    public boolean isAuthenticated(String sessionId) {
+        return authSer.isAuthenticated(sessionId);
+    }
+
+    public Optional<Wallet> getAuthenticatedWallet(String sessionId) {
+        return authSer.getAuthenticatedWallet(sessionId);
+    }
+
+    // acc management methods
+    public AuthResult changePassword(String sessionId, String currentPswrd, String newPassword) {
+        return authSer.changePassword(sessionId, currentPswrd, newPassword);
+    }
+
+    public AuthResult deleteWallet(String sessionId, String pswrd) {
+        return authSer.deleteWallet(sessionId, pswrd);
+    }
+}
