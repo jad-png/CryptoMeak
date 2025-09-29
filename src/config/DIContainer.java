f47f0ca01d505214f112d37a7c2fc6dc6b76cd72
jad-png <j.belassiria3993@uca.ac.ma>
2025-09-29 13:07:30 +0200

    Feat(): add service layers classes into DIContainer to ez dependecies management

--- src/config/DIContainer.java
+++ src/config/DIContainer.java
@@ -7,17 +7,28 @@ import repository.WalletRepository;
 import repository.interfaces.IAuthRepository;
 import repository.interfaces.ITransactionRepository;
 import repository.interfaces.IWalletRepository;
+import service.impl.AuthServiceImpl;
+import service.impl.TransactionService;
+import service.impl.WalletService;
 import utils.TransactionUtils;
 import utils.WalletUtils;
 
 import java.sql.SQLException;
 
+import controller.TransactionController;
+
 public class DIContainer {
     private static DIContainer instance;
     private final ITransactionRepository txrepo;
     private final IWalletRepository wtRepo;
-    private final UtilsFactory UtFactory;
     private final IAuthRepository authRepo;
+    
+    private final UtilsFactory UtFactory;
+
+    private final TransactionService txSer;
+    private final WalletService wtSer;
+    private final AuthServiceImpl authSer;
+
 
     private DIContainer () throws SQLException {
         this.txrepo = new TransactionRepository();
@@ -25,6 +36,10 @@ public class DIContainer {
         this.authRepo = new AuthRepositoryImpl();
 
         this.UtFactory = new UtilsFactory(txrepo, wtRepo);
+
+        this.txSer = new TransactionService();
+        this.wtSer = new WalletService();
+        this.authSer = new AuthServiceImpl();
     }
 
     public static synchronized DIContainer getInstance() throws SQLException {
@@ -55,5 +70,17 @@ public class DIContainer {
 
     public UtilsFactory getUtilsFactory() {
         return UtFactory;
+    }
+
+    public TransactionService getTxSer() {
+        return txSer;
+    }
+
+    public WalletService getWtSer() {
+        return wtSer;
+    }
+
+    public AuthServiceImpl getAuthSer() {
+        return authSer;
     }
 }
