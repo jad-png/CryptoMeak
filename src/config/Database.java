b90bf0e0c5a50c5b1ee0716efa7164c69d1abeeb
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-03 03:12:06 +0200

    feat():CRYP-115 Implement showWelcome() method

--- src/config/Database.java
+++ src/config/Database.java
@@ -8,11 +8,21 @@ public class Database {
     private static Database instance;
     private Connection conn;
 
-    private static final String URL = "";
-    private static final String USER = "";
+    private static final String URL = "jdbc:postgresql://localhost:5432/CryptoMeak";
+    private static final String USER = "jad99";
     private static final String PASS = "";
 
-    private Database() {}
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("PostgreSQL JDBC Driver not found.");
+            e.printStackTrace();
+        }
+    }
+
+    private Database() {
+    }
 
     public static Database getInstance() {
         if (instance == null) {
@@ -24,7 +34,8 @@ public class Database {
         }
         return instance;
     }
-// TODO: static block
+
+    // TODO: static block
     public Connection getConn() throws SQLException {
         if (conn == null || conn.isClosed()) {
             conn = DriverManager.getConnection(URL, USER, PASS);
